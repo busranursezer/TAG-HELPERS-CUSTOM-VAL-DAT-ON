30b6aaf71ede44e8a6dc61520cfb8e13c3a78d5f
busranursezer <119038719+busranursezer@users.noreply.github.com>
2023-03-06 13:33:14 +0300

    Add project files.

---
+++ ANK11-TAG-HELPERS-CUSTOM-VALİDATİON/Custom-Validations/JoinDateValidation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ANK11_TAG_HELPERS_CUSTOM_VALİDATİON.Custom_Validations
+{
+    public class JoinDateValidation:ValidationAttribute
+    {
+
+        //bunu siz dolduracaksınız
+
+        //istenilenler => join date bugünden büyük olamaz. 10 yıllık bir şirket olduğunu varsayalım. 11 yıllık çalışan olamaz.
+    }
+}
